24322a39a52063035ac181a1bdf8c9242007d2b2
Keshav766 <75837310+Keshav766@users.noreply.github.com>
2026-04-29 13:01:22 +0530

    feat(API): implement message sending with text and image
    - connected message input to backend API
    - used FormData for sending text and image

--- frontend/src/components/MessageArea.jsx
+++ frontend/src/components/MessageArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { FaArrowLeft } from "react-icons/fa";
 import { BsEmojiGrin } from "react-icons/bs";
 import { FaImages } from "react-icons/fa";
@@ -9,14 +9,48 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedUser } from '../redux/userSlice.js';
 import { messagearea } from '../styles/home.js';
 import SenderMessage from './SenderMessage.jsx';
-import ReceiverMessage from './ReceiverMessage';
+import ReceiverMessage from './ReceiverMessage.jsx';
+import axios from 'axios';
+import { serverURL } from '../main.jsx';
 
 function MessageArea() {
   const { selectedUser } = useSelector(state => state.user)
   const dispatch = useDispatch()
   const [showPicker, setShowPicker] = useState(false)
   const [input, setInput] = useState("")
- 
+  const [frontendImage, setFrontendImage] = useState("")
+  const [backendImage, setBackendImage] = useState("")
+  const image = useRef()
+
+  const handleImage = (e) => {
+    const file = e.target.files[0]
+    setBackendImage(file)
+    setFrontendImage(URL.createObjectURL(file))
+  }
+
+  const handleSendingMessage = async (e) => {
+    e.preventDefault()
+    try {
+      const formData = new FormData()
+
+      formData.append("message", input)
+      if (backendImage) {
+        formData.append("image", backendImage)
+      }
+
+      const result = await axios.post(
+        `${serverURL}/api/message/send/${selectedUser._id}`,
+        formData,
+        { withCredentials: true })
+      console.log(result.data)
+      setInput("")
+      setFrontendImage("")
+      setBackendImage(null)
+
+    } catch (error) {
+      console.log(`handleSendingMessage Error: ${error.message}`)
+    }
+  }
 
   const onEmojiSelect = (emojiData) => {
     setInput(prev => prev + emojiData.emoji)
@@ -59,8 +93,6 @@ function MessageArea() {
             }
             <SenderMessage />
             <ReceiverMessage />
-            <SenderMessage />
-            <ReceiverMessage />
           </div>
         </div>
       }
@@ -80,7 +112,16 @@ function MessageArea() {
         </div>
       }
       {selectedUser && <div className=' w-full h-25 lg:w-[70%] fixed bottom-5 flex items-center justify-center'>
-        <form className='bg-[#34c2f1] w-[95%] lg:w-[70%] h-15 rounded-full shadow-gray-400 shadow-lg flex justify-center items-center p-4 gap-5'>
+        {frontendImage && <img
+          src={frontendImage}
+          alt="image"
+          className='absolute w-20 bottom-25 right-[20%] rounded-lg shadow-gray-500 shadow-lg'
+        />
+        }
+        <form
+          className='bg-[#34c2f1] w-[95%] lg:w-[70%] h-15 rounded-full shadow-gray-400 shadow-lg flex justify-center items-center p-4 gap-5 relative'
+          onSubmit={handleSendingMessage}
+        >
           <div
             className='cursor-pointer'
             onClick={() => setShowPicker(prev => !prev)}>
@@ -89,22 +130,35 @@ function MessageArea() {
             />
           </div>
           <input
+            type="file"
+            accept='image/*'
+            hidden
+            ref={image}
+            onChange={handleImage}
+          />
+          <input
             type="text"
             placeholder='Message'
             value={input}
             className='w-full h-full px-4 outline-none border-0 text-[19px] text-white placeholder-white'
             onChange={(e) => setInput(e.target.value)}
           />
-          <div className='cursor-pointer' >
+          <div
+            className='cursor-pointer'
+            onClick={() => image.current.click()}
+          >
             <FaImages
               className='w-6.5 h-6.5 text-white'
             />
           </div>
-          <div className='cursor-pointer' >
+          <button
+            type='submit'
+            className='cursor-pointer'
+          >
             <IoSend
               className='w-6.5 h-6.5 text-white'
             />
-          </div>
+          </button>
         </form>
       </div>}
 
